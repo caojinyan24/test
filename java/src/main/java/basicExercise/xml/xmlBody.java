21a60a64489fc2890d89c986665ca282caceffd5
caojinyan <caojinyan@bytedance.com>
2019-08-09 16:42:15 +0800

    xml parse
    Change-Id: Id03b740cc3fd45870decc2296cbc3ee2fbbdf86b

---
+++ java/src/main/java/basicExercise/xml/xmlBody.java
@@ -0,0 +1,46 @@
+package basicExercise.xml;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "paymentService")
+public class xmlBody {
+
+    @XmlAttribute
+    private String version; // 版本号
+
+    @XmlAttribute
+    private String merchantCode; // 商户号
+
+    private Notify notify; // 通知信息
+
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Notify {
+        private OrderStatusEvent orderStatusEvent; // 订单状态通知
+
+        @Data
+        @XmlAccessorType(XmlAccessType.FIELD)
+        public static class OrderStatusEvent {
+
+            @XmlAttribute
+            private String orderCode; // 订单号
+
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "xmlBody{" +
+                "version='" + version + '\'' +
+                ", merchantCode='" + merchantCode + '\'' +
+                ", notify=" + notify +
+                '}';
+    }
+}
